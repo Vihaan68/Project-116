5a0d474bb8170d23ca13742327b1e371069f65dc
Vihaan68 <93758634+Vihaan68@users.noreply.github.com>
2023-06-01 12:51:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX=0;
+noseY=0;
+function preload(){
+    mustache=loadImage('https://i.postimg.cc/j28SyBBz/m.png');
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0,0,300,300);
+    image(mustache, noseX, noseY, 30,30)
+}
+
+function take_snapshot(){
+    save('Mustache.png');
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized ");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-0;
+        noseY=results[0].pose.nose.y-0;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
